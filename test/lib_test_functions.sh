1b803541c291cda8bc7b40b43ad7914ab6128f68
Neil Walker <neil@wynded.co.uk>
2024-01-11 11:39:03 +0000

    Refactor test code and improve readability

---
+++ test/lib_test_functions.sh
@@ -0,0 +1,35 @@
+# lib_test_functions.sh
+# Incorporate into test scripts with  . $TESTDIR/lib_test_functions.sh
+
+check_exit_code() {
+  ACTUAL=$1
+  EXPECTED=$2
+  if [ "$EXPECTED" -ne "$ACTUAL" ]; then
+    echo Expected exit code $EXPECTED, got $ACTUAL >> $LOGFILE
+    exit 1
+  fi
+  return 0
+}
+
+
+check_file_is_empty() {
+  FILENAME=$1  # the file that should be empty
+  TEXT_SOURCE=$2  # a human-readable description of where the file content came from (e.g. "stdout" or "stderr")
+  if [ -n "$(cat $FILENAME)" ]; then
+    echo "Got unexpected output on $TEXT_SOURCE" >> $LOGFILE
+    echo "--- begin $TEXT_SOURCE ---" >> $LOGFILE
+    cat "$FILENAME" >> $LOGFILE
+    echo "--- end $TEXT_SOURCE ---" >> $LOGFILE
+    exit 1
+  fi
+}
+
+
+check_file_is_not_empty() {
+  FILENAME=$1  # the file that should contain something
+  TEXT_SOURCE=$2  # a human-readable description of where the file content came from (e.g. "stdout" or "stderr")
+  if [ -z "$(cat $FILENAME)" ]; then
+    echo "Expected output on $TEXT_SOURCE but got nothing" >> $LOGFILE
+    exit 1
+  fi
+}
